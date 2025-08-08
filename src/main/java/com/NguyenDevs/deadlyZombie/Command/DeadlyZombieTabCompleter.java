8ad4b46074d13d208cdb031d87467e714aacada0
Nguyên Devs <144598460+NguyenDevs@users.noreply.github.com>
2025-08-08 22:27:33 +0700

    Create DeadlyZombieTabCompleter.java

---
+++ src/main/java/com/NguyenDevs/deadlyZombie/Command/DeadlyZombieTabCompleter.java
@@ -0,0 +1,18 @@
+package com.NguyenDevs.deadlyZombie.Command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.Collections;
+import java.util.List;
+
+public class DeadlyZombieTabCompleter implements TabCompleter {
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 1) {
+            return Collections.singletonList("reload");
+        }
+        return Collections.emptyList();
+    }
+}
